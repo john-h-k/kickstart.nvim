bf474136bccb8722017d0d9489e93100b62f4201
John Kelly <johnharrykelly@gmail.com>
2023-07-11 15:41:40 +0100

    Add lazygit keybinding to gg

--- init.lua
+++ init.lua
@@ -303,6 +303,9 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
+-- setup mapping to call :LazyGit
+vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>', {noremap = true, silent = true})
+
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
